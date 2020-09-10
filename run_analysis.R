01510df910388e84571dc96c0e5e2475fdabd387
Bradley Burquest <burquest@gmail.com>
2020-09-10 19:10:02 -0400

    Update comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,13 @@
+# Analysis script for Getting and Cleaning Data course project
+
+# Load needed libraries
 library(data.table)
 library(dplyr)
 
-# 1. Merges the training and the test sets to create one data set.
+# 1. Merge the training and the test sets to create one data set.
 colnames <- read.table("./data/features.txt")
 colnames <- colnames[ , "V2"]
-
+# read in the data
 traindatasetx <- read.table("./data/train/x_train.txt", col.names=colnames)
 testdatasetx <- read.table("./data/test/x_test.txt", col.names=colnames)
 traindatasety <- read.table("./data/train/y_train.txt", col.names=c("activity"))
@@ -13,7 +16,7 @@ activities <- read.table("./data/activity_labels.txt")
 trainsubjects <- read.table("./data/train/subject_train.txt", col.names=c("subject"))
 testsubjects <- read.table("./data/test/subject_test.txt", col.names = c("subject"))
 
-# combined datasets
+# combined datasets 
 combined_dataset <- rbind( traindatasetx, testdatasetx)
 combined_activities <-rbind( traindatasety, testdatasety)
 combined_subjects <- rbind( trainsubjects, testsubjects)
@@ -27,6 +30,7 @@ extracted_data <- cbind(combined_activities, extracted_data)
 extracted_data <- cbind(combined_subjects, extracted_data)
 
 # 3. Uses descriptive activity names to name the activities in the data set
+# Update activites column with descriptive names
 extracted_data[ ,"activity"] <- activities[extracted_data[, "activity"], "V2"]
 
 # 4. Appropriately label the data set with descriptive variable names.
@@ -45,18 +49,20 @@ for( subj in 1:30) {
    for( act in activities[ , "V2"]) {
       # select the rows for the subject and activities
       selected <- filter( extracted_data, subject==subj & activity==act)
-      # create a vector for the means for the selected columns
+      # create a vector for the mean/average for the selected columns
       vec <- as.vector(sapply(selected[,3:68], mean))
-      # create a data vector 
+      # create a data vector for the row data
       vec <- c(subj, act, vec)
-      # added the vector to the data frame
+      # added the row to the data frame
       tidydata <- rbind( tidydata, vec)
    }
 }
 # set the column names
 cnames <- paste(names(extracted_data), "_avg", sep="")
+# update subject and activity column names
 cnames[1:2] <- c("subject", "activity")
 colnames(tidydata) <- cnames
 
 # Write out the tidy data file
 write.csv(tidydata, "tidydata.csv", row.names=FALSE)
+
